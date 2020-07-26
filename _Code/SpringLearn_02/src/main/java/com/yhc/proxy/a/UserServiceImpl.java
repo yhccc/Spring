204c3c2c8b41b0c83f8b35c83d7f55a92b65e3e1
yhccc <yinhc0129@163.com>
2020-07-26 17:37:32 +0800

    update to ch.16

--- _Code/SpringLearn_02/src/main/java/com/yhc/proxy/a/UserServiceImpl.java
+++ _Code/SpringLearn_02/src/main/java/com/yhc/proxy/a/UserServiceImpl.java
@@ -4,6 +4,7 @@ public class UserServiceImpl implements IUserService {
     @Override
     public void register(User user) {
         System.out.println("UserServiceImpl.register");
+//        throw new RuntimeException("测试异常");
     }
 
     @Override
